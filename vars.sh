41022cea50497a40ba7abde0d55ad05c439895cb
cpxazn <cpxazn@gmail.com>
2022-07-20 11:25:30 -0400

    initial commit

---
+++ vars.sh
@@ -0,0 +1,9 @@
+#Email to send from
+export email_user=test@gmail.com
+export email_pwd=
+
+#Email recipient for print completion
+export email_to=test@gmail.com
+
+#Email recipient for filament runout
+export email_to2=test@gmail.com,0123456789@vtext.com
